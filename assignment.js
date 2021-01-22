b0891289b28947181d117b4334166b803cde4d5e
Latifur Rahman <latifurrahman350@gmail.com>
2021-01-22 12:03:49 +0600

    4 no question solved

--- assignment.js
+++ assignment.js
@@ -1,34 +1,39 @@
 
 // kilometer to meter part start-----------
 
-function kilometerToMeter(kilometer){
-    const meter = kilometer * 1000;
-    return meter;
-}
+// function kilometerToMeter(kilometer){
+//     const meter = kilometer * 1000;
+//     return meter;
+// }
 
-let result = kilometerToMeter(100);
-console.log(result);
+// let result = kilometerToMeter(100);
+// console.log(result);
 
 
 
 
 // budget calculator part start-------------
 
+function budgetCalculator(watch, phone, laptop){
+  var result = (( watch * 7 ) + ( phone * 7 ) + ( laptop * 7 ));
+  return result;
+}
 
+var total = budgetCalculator( 50, 100, 500 );
+console.log(total);
 
 
 
 // mega friend part start------------
-
-function findLongestWord(str) {
-    var strSplit = str.split(' ');
-    var longestWord = 0;
-    for(var i = 0; i < strSplit.length; i++){
-      if(strSplit[i].length > longestWord){
-      longestWord = strSplit[i].length;
-       }
-    }
-    return longestWord;
-  }
-  var output = findLongestWord("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
-  console.log(output);
+// function findLongestWord(str) {
+//     var strSplit = str.split(" ");
+//     var longestWord = 0;
+//     for(var i = 0; i < strSplit.length; i++){
+//       if(strSplit[i].length > longestWord.legth){
+//       longestWord = strSplit[i].length;
+//        }
+//     }
+//     return longestWord;
+//   }
+//   console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+  
